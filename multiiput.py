7f4195cb4640ce11662f4e774a0f7575c79a0d5e
Vybhav Jain <35528017+vybhavjain@users.noreply.github.com>
2019-02-11 08:25:47 +0530

    Create multiiput.py

---
+++ multiiput.py
@@ -0,0 +1,18 @@
+# Incomplete
+
+l = []
+l1 = []
+k,l=input().split()
+n = int(k)
+k = int(l)
+print(n)         
+print(k)
+t = n-1        
+for i in range(t):
+    m,l=input().split()
+    l1[i].append(int(m))
+    l1[i].append(int(l))
+    
+    
+print(l1)
+
