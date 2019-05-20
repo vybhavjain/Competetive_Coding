8d860104f1a3594c1e23a6c4aeda21f5ee401c39
Vybhav Jain <35528017+vybhavjain@users.noreply.github.com>
2019-05-20 21:46:04 +0530

    Update DeletingNumbers.py

--- DeletingNumbers.py
+++ DeletingNumbers.py
@@ -10,18 +10,14 @@ for i in range(n):
     
 middle = (n+1)//2
 if k > middle:
-    for t in range(k-middle):
+    for t in range(middle):
         arr.append(0)
 max1 = 0
-max2 = 0
-final_max = 0
 k = k-1
 for i in range(k):
     if arr[middle-i]>max1:
         max1 = arr[middle-i]
-        #print(max1)
 for i in range(k):
-    if arr[middle+i]>max2:
-        max2 = arr[middle+i]
-        #print(max2)
-print(max(max1,max2,arr[middle]))
+    if arr[middle+i]>max1:
+        max1 = arr[middle+i]
+print(max(max1,arr[middle]))
