33ed51e0b89cb0e51ceb986e7bb9cd9665a8e134
Vybhav Jain <35528017+vybhavjain@users.noreply.github.com>
2020-01-21 22:57:56 +0530

    Update equal_subarray.py

--- equal_subarray.py
+++ equal_subarray.py
@@ -1,17 +1,18 @@
 n = int(input())
 k1 = int(input())
 A = list(map(int,input().split(" ")))
-out = []
-for i in range(0,n):
-    for j in range(1,n):
+t = 0
+for i in range(n,1,-1):
+    for j in range(0,n):
         if j + i <=n:
             sum2 = 0
-            trial = A[i:j+i]
+            trial = A[j:j+i]
             max1 = max(trial)
-            for k in range(len(trial)):
-                sum2 = sum2 + max1 - trial[k]
-            #print(trial,sum2,len(trial))
+            sum2 = max1*len(trial) - sum(trial)
             if sum2<=k1:
-                out.append(len(trial))
-print(max(out))
+                t = 1
+                break
+    if t == 1:
+        break
+print(len(trial))
         
