6de18566c8d25bc1164c31b38dc0004bdb7c4f55
Vybhav Jain <35528017+vybhavjain@users.noreply.github.com>
2020-01-25 15:15:42 +0530

    Update set_numbers.py

--- set_numbers.py
+++ set_numbers.py
@@ -1,8 +1,15 @@
 import math
+
 t = int(input())
 
 for i in range(t):
     n = int(input())
-    pos =  math.ceil(math.log2(n))
-    p   = pow(2, pos-1)
-    print(p-1)
+    x =  math.log2(n)
+    pos = math.ceil(x)
+    if math.log2(n+1) % 1 == 0:
+        print(n)
+    elif x%1 !=0:
+        p   = pow(2, pos-1)
+        print(p-1)
+    else:
+        print(n-1)
